8f92ad9bbf55ac37c98ad90b236b1470713054fc
suryatejaj97 <suryatejaj97@gmail.com>
2021-07-02 01:08:30 +0530

    socket connection

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const app = require("express")();
 
 const server = require("http").createServer(app);
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
 io.on("connection", (socket) => {
   console.log("what is scoket:", socket);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { nanoid } from "nanoid";
+
+const socket = io.connect("https://localhost:5000");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+  
+  const sendChat = (e) => {
+    e.preventDefault()
+    socket.emit("chat", {message})
+    setMessage("")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Socket.io application</h1>
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="send text"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+          <button type="submit">Send</button>
+        </form>
       </header>
     </div>
   );
