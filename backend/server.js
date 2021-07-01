cdb0add771fe26eff62c8a7baa85fe1bc1c369ab
suryatejaj97 <suryatejaj97@gmail.com>
2021-07-01 22:18:38 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const app = require("express")();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("what is scoket:", socket);
+  console.log("socket is active");
+
+  socket.on("chat", (payload) => {
+    console.log("what is payload ", payload);
+    io.emit("chat", payload);
+  });
+});
+
+// app.listen(5000, () => console.log("server is active"))
+
+server.listen(5000, () => console.log("server is active at 5000...."))
